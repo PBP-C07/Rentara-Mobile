c3e591875a53fd852aeba7e3bdd193c9d8fc6bd1
freiazulaika <freiazulaika27@gmail.com>
2024-12-15 00:24:39 +0700

    merapihkan tampilan katalog

--- lib/pages/sewajual/screens/user/catalogue.dart
+++ lib/pages/sewajual/screens/user/catalogue.dart
@@ -8,6 +8,7 @@ import '../../models/vehicle_model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'detail_product.dart';
 import '../../../main/screens/login.dart';
+import '../../widgets/user/car_card.dart';
 
 class CarCatalogueScreen extends StatefulWidget {
   const CarCatalogueScreen({Key? key}) : super(key: key);
@@ -141,183 +142,36 @@ class _CarCatalogueScreenState extends State<CarCatalogueScreen> {
                                       itemBuilder: (context, index) {
                                         final vehicle = snapshot.data![index];
                                         return InkWell(
-                                            onTap: () async {
-                                              final prefs =
-                                                  await SharedPreferences
-                                                      .getInstance();
-                                              final username =
-                                                  prefs.getString('username');
+                                          onTap: () async {
+                                            final prefs =
+                                                await SharedPreferences
+                                                    .getInstance();
+                                            final username =
+                                                prefs.getString('username');
 
-                                              if (context.mounted) {
-                                                if (username != null) {
-                                                  // User sudah login, navigate ke detail
-                                                  Navigator.push(
-                                                    context,
-                                                    MaterialPageRoute(
-                                                      builder: (context) =>
-                                                          CarDetailScreen(
-                                                              vehicle: vehicle),
-                                                    ),
-                                                  );
-                                                } else {
-                                                  Navigator.push(
-                                                    context,
-                                                    MaterialPageRoute(
-                                                      builder: (context) =>
-                                                          const LoginPage(),
-                                                    ),
-                                                  );
-                                                }
+                                            if (context.mounted) {
+                                              if (username != null) {
+                                                Navigator.push(
+                                                  context,
+                                                  MaterialPageRoute(
+                                                    builder: (context) =>
+                                                        CarDetailScreen(
+                                                            vehicle: vehicle),
+                                                  ),
+                                                );
+                                              } else {
+                                                Navigator.push(
+                                                  context,
+                                                  MaterialPageRoute(
+                                                    builder: (context) =>
+                                                        const LoginPage(),
+                                                  ),
+                                                );
                                               }
-                                            },
-                                            child: Card(
-                                              margin: const EdgeInsets.only(
-                                                  bottom: 16),
-                                              shape: RoundedRectangleBorder(
-                                                borderRadius:
-                                                    BorderRadius.circular(12),
-                                              ),
-                                              child: Column(
-                                                crossAxisAlignment:
-                                                    CrossAxisAlignment.start,
-                                                children: [
-                                                  ClipRRect(
-                                                    borderRadius:
-                                                        const BorderRadius
-                                                            .vertical(
-                                                      top: Radius.circular(12),
-                                                    ),
-                                                    child: Image.network(
-                                                      vehicle.fields.linkFoto,
-                                                      height: 200,
-                                                      width: double.infinity,
-                                                      fit: BoxFit.cover,
-                                                      errorBuilder: (context,
-                                                          error, stackTrace) {
-                                                        return Container(
-                                                          height: 200,
-                                                          color:
-                                                              Colors.grey[200],
-                                                          child: const Center(
-                                                            child: Icon(
-                                                                Icons
-                                                                    .error_outline,
-                                                                size: 40),
-                                                          ),
-                                                        );
-                                                      },
-                                                    ),
-                                                  ),
-                                                  Padding(
-                                                    padding:
-                                                        const EdgeInsets.all(
-                                                            16),
-                                                    child: Column(
-                                                      crossAxisAlignment:
-                                                          CrossAxisAlignment
-                                                              .start,
-                                                      children: [
-                                                        Row(
-                                                          mainAxisAlignment:
-                                                              MainAxisAlignment
-                                                                  .spaceBetween,
-                                                          crossAxisAlignment:
-                                                              CrossAxisAlignment
-                                                                  .start,
-                                                          children: [
-                                                            Expanded(
-                                                              flex: 2,
-                                                              child: Text(
-                                                                '${vehicle.fields.merk} ${vehicle.fields.tipe}',
-                                                                style:
-                                                                    const TextStyle(
-                                                                  fontSize: 18,
-                                                                  fontWeight:
-                                                                      FontWeight
-                                                                          .bold,
-                                                                ),
-                                                                maxLines: 2,
-                                                                overflow:
-                                                                    TextOverflow
-                                                                        .ellipsis,
-                                                              ),
-                                                            ),
-                                                            Container(
-                                                              margin:
-                                                                  const EdgeInsets
-                                                                      .only(
-                                                                      left: 8),
-                                                              padding:
-                                                                  const EdgeInsets
-                                                                      .symmetric(
-                                                                horizontal: 12,
-                                                                vertical: 6,
-                                                              ),
-                                                              decoration:
-                                                                  BoxDecoration(
-                                                                color: const Color(
-                                                                    0xFF2B6777),
-                                                                borderRadius:
-                                                                    BorderRadius
-                                                                        .circular(
-                                                                            20),
-                                                              ),
-                                                              child: Text(
-                                                                formatPrice(
-                                                                    vehicle
-                                                                        .fields
-                                                                        .harga),
-                                                                style:
-                                                                    const TextStyle(
-                                                                  color: Colors
-                                                                      .white,
-                                                                  fontWeight:
-                                                                      FontWeight
-                                                                          .bold,
-                                                                  fontSize: 13,
-                                                                ),
-                                                              ),
-                                                            ),
-                                                          ],
-                                                        ),
-                                                        const SizedBox(
-                                                            height: 8),
-                                                        Text(
-                                                          vehicle.fields.toko,
-                                                          style: TextStyle(
-                                                            color: Colors
-                                                                .grey[600],
-                                                            fontSize: 14,
-                                                          ),
-                                                          overflow: TextOverflow
-                                                              .ellipsis,
-                                                        ),
-                                                        const SizedBox(
-                                                            height: 12),
-                                                        Row(
-                                                          children: [
-                                                            _buildFeature(
-                                                                Icons.palette,
-                                                                vehicle.fields
-                                                                    .warna),
-                                                            const SizedBox(
-                                                                width: 16),
-                                                            _buildFeature(
-                                                              Icons
-                                                                  .local_gas_station,
-                                                              bahanBakarValues
-                                                                      .reverse[
-                                                                  vehicle.fields
-                                                                      .bahanBakar]!,
-                                                            ),
-                                                          ],
-                                                        ),
-                                                      ],
-                                                    ),
-                                                  ),
-                                                ],
-                                              ),
-                                            ));
+                                            }
+                                          },
+                                          child: CarCard(vehicle: vehicle),
+                                        );
                                       },
                                     ),
                                     const SizedBox(height: 80),
